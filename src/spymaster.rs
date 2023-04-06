2bec83393d78cd78350fc15a763c6e50d90604dc
Kike <enriquelagui@outlook.com>
2023-04-06 09:31:53 +0200

    Add additional input format checking for Spymaster game

--- src/spymaster.rs
+++ src/spymaster.rs
@@ -37,39 +37,60 @@ pub fn play_spymaster_game() {
         guessed_mask: vec![vec![false; BOARD_SIZE]; BOARD_SIZE]
     };
 
-    let clue_re = Regex::new(r"\w \d").unwrap();
-    let mut clue = String::new();
     // Game loop
     loop {
         print_board(&board, true);
         print_your_words(&board);
+        let words_on_board = get_remaining_words(&board);
+        let clue_re = Regex::new(r"\w \d").unwrap();
+        let mut clue_word: String = String::new();
+        let mut clue_number = 0;
+        let mut clue_ok = false;
+
+        let remaining_words = get_remaining_words(&board);
 
         // Get clue from user
-        while !clue_re.is_match(&clue) {
+        while !clue_ok {
             println!("Provide a clue:");
-                clue = read_user_input();
+            let clue = read_user_input();
+
+            if !clue_re.is_match(&clue) {
+                println!("Invalid format. Format should be: WORD NUM \n");
+                continue;
+            }
+
+            let clue_parts: Vec<&str> = clue.trim().split(' ').collect();
+            clue_word = clue_parts[0].to_string();
+            clue_number = clue_parts[1]
+                .trim()
+                .parse()
+                .expect("Last value of the clue is not an integer");
+
+            if words_on_board.contains(&clue_word) {
+                println!("You can't use a word that's on the board! \n");
+                continue;
+            }
+
+            let n_team_words = get_remaining_team_words(&board).len();
+            if clue_number > n_team_words {
+                println!(
+                    "The clue number can't be higher than the number of remaining team words ({n_team_words}). \n");
+                continue;
+            }
+            clue_ok = true;
         }
-        let clue_parts: Vec<&str> = clue.trim().split(' ').collect();
-        let reference_word: String = clue_parts[0].to_string();
-        let n_words_referenced: usize = clue_parts[clue_parts.len() - 1]
-            .trim()
-            .parse()
-            .expect("Last value of the clue is not an integer");
-        let remaining_words = get_remaining_words(&board);
+
         let result: Vec<f32> = compute_word_to_words_similarity(
-            &reference_word, 
+            &clue_word, 
             &remaining_words)
             .expect("Couldn't get result from AI model");
         let max_words: Vec<String> = get_n_max_words(
-            &remaining_words, &result, n_words_referenced);
+            &remaining_words, &result, clue_number);
         println!("AI guesses: {:?}", max_words);
         cross_guessed_words(&mut board, &max_words);
         if get_remaining_team_words(&board).len() == 0 {
             break;
         }
-
-        // reset
-        clue = "".to_string();
         println!(" ");
     }
     println!("You win!");
