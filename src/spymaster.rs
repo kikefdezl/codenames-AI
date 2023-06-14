84f926381d737627112a49f78f638f606990aea1
Kike <enriquelagui@outlook.com>
2023-06-14 22:48:45 +0200

    Autoformat

--- src/spymaster.rs
+++ src/spymaster.rs
@@ -1,40 +1,25 @@
 use regex::Regex;
 
-use crate::constants:: BOARD_SIZE; 
-use crate::common::{ 
-    Board, 
-    print_board, 
-    print_your_words,
-    get_team_mask, 
-    get_word_board, 
-    get_remaining_words,
-    get_remaining_team_words,
-    read_user_input, 
-    cross_guessed_words,
-    compute_word_to_words_similarity
+use crate::common::{
+    compute_word_to_words_similarity, cross_guessed_words, get_remaining_team_words,
+    get_remaining_words, get_team_mask, get_word_board, print_board, print_your_words,
+    read_user_input, Board,
 };
+use crate::constants::BOARD_SIZE;
 
-
-fn get_n_max_words(words: &Vec<String>, 
-                   values: &Vec<f32>, 
-                   n: usize) -> Vec<String> {
+fn get_n_max_words(words: &Vec<String>, values: &Vec<f32>, n: usize) -> Vec<String> {
     let mut pairs: Vec<_> = words.iter().zip(values.iter()).collect();
     pairs.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
 
-    let max_words: Vec<String> = pairs
-        .iter()
-        .take(n)
-        .map(|(s, _)| s.to_string())
-        .collect();
+    let max_words: Vec<String> = pairs.iter().take(n).map(|(s, _)| s.to_string()).collect();
     return max_words;
 }
-
 
 pub fn play_spymaster_game() {
     let mut board = Board {
         words: get_word_board(),
         team_mask: get_team_mask(),
-        guessed_mask: vec![vec![false; BOARD_SIZE]; BOARD_SIZE]
+        guessed_mask: vec![vec![false; BOARD_SIZE]; BOARD_SIZE],
     };
 
     // Game loop
@@ -80,12 +65,9 @@ pub fn play_spymaster_game() {
             clue_ok = true;
         }
 
-        let result: Vec<f32> = compute_word_to_words_similarity(
-            &clue_word, 
-            &remaining_words)
+        let result: Vec<f32> = compute_word_to_words_similarity(&clue_word, &remaining_words)
             .expect("Couldn't get result from AI model");
-        let max_words: Vec<String> = get_n_max_words(
-            &remaining_words, &result, clue_number);
+        let max_words: Vec<String> = get_n_max_words(&remaining_words, &result, clue_number);
         println!("AI guesses: {:?}", max_words);
         cross_guessed_words(&mut board, &max_words);
         if get_remaining_team_words(&board).len() == 0 {
