d0442b960336168191645f4e3381da94ae8a4441
kike <efernandez@aisuperior.com>
2024-01-14 18:57:53 +0100

    Add better quantized version of Mistral 7b Instruct

--- src/ai/mistral.rs
+++ src/ai/mistral.rs
@@ -1,181 +1,131 @@
-#[cfg(feature = "mkl")]
-extern crate intel_mkl_src;
-
-#[cfg(feature = "accelerate")]
-extern crate accelerate_src;
-
-use anyhow::{Error as E, Result};
-
-use candle_transformers::models::mistral::Config;
-use candle_transformers::models::quantized_mistral::Model as QMistral;
-
-use candle_core::{DType, Device, Tensor};
-use candle_examples::token_output_stream::TokenOutputStream;
+use candle_core::quantized::gguf_file;
+use candle_core::{Device, Tensor};
 use candle_transformers::generation::LogitsProcessor;
-use hf_hub::{api::sync::Api, Repo, RepoType};
+use std::io::Write;
 use tokenizers::Tokenizer;
 
-const USE_FLASH_ATTN: bool = false;
-const TEMPERATURE: Option<f64> = Some(0.0);
-const TOP_P: Option<f64> = None;
-const SEED: u64 = 299792458;
-const SAMPLE_LEN: usize = 100;
-const MODEL_ID: &str = "lmz/candle-mistral";
-const REVISION: &str = "main";
-const MODEL_FILENAME: &str = "model-q4k.gguf";
+use candle_examples::token_output_stream::TokenOutputStream;
+use candle_transformers::models::quantized_llama as model;
+use model::ModelWeights;
+
+const MODEL_REPO: &str = "TheBloke/Mistral-7B-Instruct-v0.1-GGUF";
+const MODEL_FILENAME: &str = "mistral-7b-instruct-v0.1.Q4_0.gguf";
+const TOKENIZER_REPO: &str = "mistralai/Mistral-7B-v0.1";
 const TOKENIZER_FILENAME: &str = "tokenizer.json";
+const TEMPERATURE: Option<f64> = Some(0.1);
 const REPEAT_PENALTY: f32 = 1.1;
 const REPEAT_LAST_N: usize = 64;
+const SAMPLE_LEN: usize = 1000;
+const SEED: u64 = 299792458;
+const TOP_P: Option<f64> = None;
 
-enum Model {
-    Quantized(QMistral),
-}
-
-struct TextGeneration {
-    model: Model,
-    device: Device,
-    tokenizer: TokenOutputStream,
-    logits_processor: LogitsProcessor,
-    repeat_penalty: f32,
-    repeat_last_n: usize,
-}
-
-impl TextGeneration {
-    #[allow(clippy::too_many_arguments)]
-    fn new(
-        model: Model,
-        tokenizer: Tokenizer,
-        seed: u64,
-        temp: Option<f64>,
-        top_p: Option<f64>,
-        repeat_penalty: f32,
-        repeat_last_n: usize,
-        device: &Device,
-    ) -> Self {
-        let logits_processor = LogitsProcessor::new(seed, temp, top_p);
-        Self {
-            model,
-            tokenizer: TokenOutputStream::new(tokenizer),
-            logits_processor,
-            repeat_penalty,
-            repeat_last_n,
-            device: device.clone(),
-        }
+fn format_size(size_in_bytes: usize) -> String {
+    if size_in_bytes < 1_000 {
+        format!("{}B", size_in_bytes)
+    } else if size_in_bytes < 1_000_000 {
+        format!("{:.2}KB", size_in_bytes as f64 / 1e3)
+    } else if size_in_bytes < 1_000_000_000 {
+        format!("{:.2}MB", size_in_bytes as f64 / 1e6)
+    } else {
+        format!("{:.2}GB", size_in_bytes as f64 / 1e9)
     }
+}
 
-    fn run(&mut self, prompt: &str, sample_len: usize) -> Result<()> {
-        use std::io::Write;
-        self.tokenizer.clear();
-        let mut tokens = self
-            .tokenizer
+pub fn prompt_mistral(prompt: &str) -> anyhow::Result<()> {
+    let api = hf_hub::api::sync::Api::new()?;
+    let api = api.model(MODEL_REPO.to_string());
+    let model_path = api.get(MODEL_FILENAME)?;
+    let mut file = std::fs::File::open(&model_path)?;
+
+    let model = gguf_file::Content::read(&mut file).map_err(|e| e.with_path(model_path))?;
+    let mut total_size_in_bytes = 0;
+    for (_, tensor) in model.tensor_infos.iter() {
+        let elem_count = tensor.shape.elem_count();
+        total_size_in_bytes +=
+            elem_count * tensor.ggml_dtype.type_size() / tensor.ggml_dtype.blck_size();
+    }
+    println!(
+        "loaded {:?} tensors ({})",
+        model.tensor_infos.len(),
+        &format_size(total_size_in_bytes),
+    );
+    let mut model = ModelWeights::from_gguf(model, &mut file)?;
+    println!("Model built!");
+
+    let api = hf_hub::api::sync::Api::new()?;
+    let api = api.model(TOKENIZER_REPO.to_string());
+    let tokenizer_path = api.get(TOKENIZER_FILENAME)?;
+    let tokenizer = Tokenizer::from_file(tokenizer_path).map_err(anyhow::Error::msg)?;
+
+    let mut tos = TokenOutputStream::new(tokenizer);
+    let prompt = format!("<s>[INST] {} [/INST]", prompt).to_string();
+
+    let pre_prompt_tokens = vec![];
+
+    loop {
+        print!("{}", &prompt);
+        let tokens = tos
             .tokenizer()
             .encode(prompt, true)
-            .map_err(E::msg)?
-            .get_ids()
-            .to_vec();
-        for &t in tokens.iter() {
-            if let Some(t) = self.tokenizer.next_token(t)? {
-                print!("{t}")
-            }
-        }
-        std::io::stdout().flush()?;
-
-        let mut generated_tokens = 0usize;
-        let eos_token = match self.tokenizer.get_token("</s>") {
-            Some(token) => token,
-            None => anyhow::bail!("cannot find the </s> token"),
+            .map_err(anyhow::Error::msg)?;
+        let prompt_tokens = [&pre_prompt_tokens, tokens.get_ids()].concat();
+        let to_sample = SAMPLE_LEN.saturating_sub(1);
+        let prompt_tokens = if prompt_tokens.len() + to_sample > model::MAX_SEQ_LEN - 10 {
+            let to_remove = prompt_tokens.len() + to_sample + 10 - model::MAX_SEQ_LEN;
+            prompt_tokens[prompt_tokens.len().saturating_sub(to_remove)..].to_vec()
+        } else {
+            prompt_tokens
         };
-        let start_gen = std::time::Instant::now();
-        for index in 0..sample_len {
-            let context_size = if index > 0 { 1 } else { tokens.len() };
-            let start_pos = tokens.len().saturating_sub(context_size);
-            let ctxt = &tokens[start_pos..];
-            let input = Tensor::new(ctxt, &self.device)?.unsqueeze(0)?;
-            let logits = match &mut self.model {
-                Model::Quantized(m) => m.forward(&input, start_pos)?,
-            };
-            let logits = logits.squeeze(0)?.squeeze(0)?.to_dtype(DType::F32)?;
-            let logits = if self.repeat_penalty == 1. {
+
+        let mut all_tokens = vec![];
+        let mut logits_processor = LogitsProcessor::new(SEED, TEMPERATURE, TOP_P);
+
+        let mut next_token = {
+            let input = Tensor::new(prompt_tokens.as_slice(), &Device::Cpu)?.unsqueeze(0)?;
+            let logits = model.forward(&input, 0)?;
+            let logits = logits.squeeze(0)?;
+            logits_processor.sample(&logits)?
+        };
+        all_tokens.push(next_token);
+        if let Some(t) = tos.next_token(next_token)? {
+            print!("{t}");
+            std::io::stdout().flush()?;
+        }
+
+        let eos_token = "</s>";
+        let eos_token = *tos.tokenizer().get_vocab(true).get(eos_token).unwrap();
+
+        for index in 0..to_sample {
+            let input = Tensor::new(&[next_token], &Device::Cpu)?.unsqueeze(0)?;
+            let logits = model.forward(&input, prompt_tokens.len() + index)?;
+            let logits = logits.squeeze(0)?;
+            let logits = if REPEAT_PENALTY == 1. {
                 logits
             } else {
-                let start_at = tokens.len().saturating_sub(self.repeat_last_n);
+                let start_at = all_tokens.len().saturating_sub(REPEAT_LAST_N);
                 candle_transformers::utils::apply_repeat_penalty(
                     &logits,
-                    self.repeat_penalty,
-                    &tokens[start_at..],
+                    REPEAT_PENALTY,
+                    &all_tokens[start_at..],
                 )?
             };
-
-            let next_token = self.logits_processor.sample(&logits)?;
-            tokens.push(next_token);
-            generated_tokens += 1;
-            if next_token == eos_token {
-                break;
-            }
-            if let Some(t) = self.tokenizer.next_token(next_token)? {
+            next_token = logits_processor.sample(&logits)?;
+            all_tokens.push(next_token);
+            if let Some(t) = tos.next_token(next_token)? {
                 print!("{t}");
                 std::io::stdout().flush()?;
             }
+            if next_token == eos_token {
+                break;
+            };
         }
-        let dt = start_gen.elapsed();
-        if let Some(rest) = self.tokenizer.decode_rest().map_err(E::msg)? {
+
+        if let Some(rest) = tos.decode_rest().map_err(candle_core::Error::msg)? {
             print!("{rest}");
         }
         std::io::stdout().flush()?;
-        println!(
-            "\n{generated_tokens} tokens generated ({:.2} token/s)",
-            generated_tokens as f64 / dt.as_secs_f64(),
-        );
-        Ok(())
+        break;
     }
-}
 
-pub fn prompt_mistral(prompt: &str) -> Result<()> {
-    use tracing_chrome::ChromeLayerBuilder;
-    use tracing_subscriber::prelude::*;
-
-    println!(
-        "avx: {}, neon: {}, simd128: {}, f16c: {}",
-        candle_core::utils::with_avx(),
-        candle_core::utils::with_neon(),
-        candle_core::utils::with_simd128(),
-        candle_core::utils::with_f16c()
-    );
-    println!(
-        "temp: {:.2} repeat-penalty: {:.2} repeat-last-n: {}",
-        TEMPERATURE.unwrap_or(0.0),
-        REPEAT_PENALTY,
-        REPEAT_LAST_N,
-    );
-
-    let api = Api::new()?;
-    let repo = api.repo(Repo::with_revision(
-        MODEL_ID.to_string(),
-        RepoType::Model,
-        REVISION.to_string(),
-    ));
-
-    let tokenizer_filename = repo.get(TOKENIZER_FILENAME)?;
-    let tokenizer = Tokenizer::from_file(tokenizer_filename).map_err(E::msg)?;
-
-    let config = Config::config_7b_v0_1(USE_FLASH_ATTN);
-    let vb = candle_transformers::quantized_var_builder::VarBuilder::from_gguf(
-        repo.get(MODEL_FILENAME)?,
-    )?;
-    let model = QMistral::new(&config, vb)?;
-
-    let (model, device) = (Model::Quantized(model), Device::Cpu);
-
-    let mut pipeline = TextGeneration::new(
-        model,
-        tokenizer,
-        SEED,
-        TEMPERATURE,
-        TOP_P,
-        REPEAT_PENALTY,
-        REPEAT_LAST_N,
-        &device,
-    );
-    pipeline.run(prompt, SAMPLE_LEN)?;
     Ok(())
 }
